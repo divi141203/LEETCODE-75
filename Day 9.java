4b439064162f5a369047adc19650fbfcf97207cd
DIVYASHRI D <118615525+divi141203@users.noreply.github.com>
2023-12-06 20:17:15 +0530

    Day 9.java

--- Day 9.java
+++ Day 9.java
@@ -1,20 +1,22 @@
 class Solution {
     public int compress(char[] chars) {
-      int i1=0,i2=0;
-      while(i1<chars.length)
+      int i1=0,i2=0; 
+      while(i1 <chars.length)
       {
-          char c=chars[i1];
+          char c1=chars[i1];
           int count=0;
-          while(i1 < chars.length && chars[i1] == c)
+          while(i1 < chars.length && chars[i1] == c1)
           {
               i1++;
               count++;
           }
-          chars[i2++]=c;
+          chars[i2++]=c1;
           if(count!=1)
           {
                 for(char c:Integer.toString(count).toCharArray())
+                {
                     chars[i2++]=c;
+                }
           }
       }
       return i2;
