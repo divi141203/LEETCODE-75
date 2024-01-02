d03570aa80681b433e84f320332cf55908041644
DIVYASHRI D <118615525+divi141203@users.noreply.github.com>
2024-01-02 20:24:52 +0530

    Day 36.java

---
+++ Day 36.java
@@ -0,0 +1,26 @@
+class Solution{
+    public int pathSum(TreeNode root, int targetSum) {
+        return solve(root, targetSum, new ArrayList<>(), 0);
+    }
+    private int solve(TreeNode root, int targetSum, ArrayList<TreeNode> path, long currentSum)
+    {
+        if (root == null) {
+            return 0;
+        }
+        path.add(root);
+        currentSum += root.val;
+        int count = 0;
+        long sum = 0;
+        for (int i = path.size() - 1; i >= 0; i--) {
+            sum += path.get(i).val;
+            if (sum == targetSum) {
+                count++;
+            }
+        }
+        count += solve(root.left, targetSum, path, currentSum);
+        count += solve(root.right, targetSum, path, currentSum);
+        path.remove(path.size() - 1);
+        currentSum -= root.val;
+        return count;
+    }
+}
